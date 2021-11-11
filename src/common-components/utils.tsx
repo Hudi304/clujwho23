5ce9f6cfc3b222bd3aef2fefbc08c433c1e6b2aa
Hudi304 <hudisteanum@yahoo.com>
2021-11-11 19:57:43 +0200

    Updates

--- src/common-components/utils.tsx
+++
@@ -1,2 +0,0 @@
-
-export const URL = "http://127.0.0.1:3030"
--- src/components/background/background-component.tsx
+++
@@ -1,5 +0,0 @@
-import './background.scss'
-
-export function BackGround(props: any): JSX.Element {
-    return <div className="backgroud-image"></div>
-}
